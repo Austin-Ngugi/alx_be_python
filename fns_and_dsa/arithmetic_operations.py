e7353a99284825869fb1bc131622866f9eeafcdf
Austin-Ngugi <79316721+Austin-Ngugi@users.noreply.github.com>
2025-09-26 14:56:02 +0300

    Add files via upload
    elif

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,19 @@
+def perform_operation(num1, num2, operation):
+    #float num1
+    #float num2
+    #str operation
+    match operation:
+        case "add":
+            return num1 + num2
+        case "subtract":
+            return num1 - num2
+        case "multiply":
+            return num1 * num2
+        case "divide":
+            if num2 == 0:
+                return("Zero division error, you cannot dividie by zero")
+            elif:
+                return num1 / num2         
+        case _:
+            print("enter a valid operator")
+            
