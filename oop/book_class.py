716d07c0d902a08dbcbbc852dcac777cb3dbc1f2
Austin-Ngugi <79316721+Austin-Ngugi@users.noreply.github.com>
2026-01-03 16:25:09 +0300

    Update book_class.py
    Corrected

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,16 +6,12 @@ class Book:
         self.author = author 
         self.year = year 
         
-    def __del__ (self, title):
-        self.title.close()
+    def __del__ (self):
         print( f"deleting {self.title}")
         
-    def _str_(self):
+    def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
-    def _repr_(self):
+    def __repr__(self):
         return f"Book('{self.title}', '{self.author}', {self.year})"
     
-    
-        
-        
